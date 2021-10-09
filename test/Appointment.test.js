b569b71bc42e67c9180c9a40e42df8f04f435f2c
Mehdi Chitforoosh <mehdichitforoosh@gmail.com>
2021-10-09 11:40:39 +0330

    Add another test and clear document body
    Clear documnet body in beforeEach method to run tests independently

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -5,15 +5,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Appointment } from '../src/components/Appointment';
 
+
 describe('Appointment', () => {
+
+    let customer;
+    let container;
+
+    beforeEach(() => {
+        container = document.createElement('div');
+        document.body.innerHTML = '';
+    })
+
     it('renders the customer first name', () => {
-        const customer = { firstName: 'Ashley' };
+        customer = { firstName: 'Ashley' };
         const component = <Appointment customer={customer} />;
-        const container = document.createElement('div');
         document.body.appendChild(container);
 
         ReactDOM.render(component, container);
 
-        expect(document.body.textContent).toMatch(customer.firstName);
+        expect(document.body.textContent).toBe(customer.firstName);
+    })
+
+    it('renders another customer first name', () => {
+        customer = { firstName: 'Jordan' }
+        const component = <Appointment customer={customer} />
+        document.body.appendChild(container);
+
+        ReactDOM.render(component, container);
+
+        expect(document.body.textContent).toBe(customer.firstName);
     })
 })

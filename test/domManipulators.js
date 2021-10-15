e4f0769087f9b45617c259e2603448aec1233733
Mehdi Chitforoosh <mehdichitforoosh@gmail.com>
2021-10-15 20:07:43 +0330

    Add test helpers an set jest test environment

---
+++ test/domManipulators.js
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import ReactDOM from "react-dom";
+
+export const createContainer = () => {
+    const container = document.createElement('div');
+
+    return {
+        render: component => ReactDOM.render(component, container),
+        container
+    }
+}
